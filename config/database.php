445c631a28caf5e4cffcfd0f91ec21cf15cdc66e
Qazi Salman Ahmad <mr.salman.ahmad@gmail.com>
2018-04-13 06:14:57 +0500

    prepared for Heroku deployment.

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-221-192-231.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd1j9dp4hq1lps6'),
+            'username' => env('DB_USERNAME', 'pqvuhsfbkausna'),
+            'password' => env('DB_PASSWORD', '544294082051ac09ef2b46049c479923bcdb286b61ad10a0d344bca4effcb66f'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
